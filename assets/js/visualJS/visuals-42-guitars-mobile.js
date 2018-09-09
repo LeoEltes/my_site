c8d3b6d7110f2e43b4a5439f41c0ccfcdee45bfc
Leo Eltes <leo@eltes.se>
2018-09-09 15:31:10 +0200

    rotationbug search

--- assets/js/visualJS/visuals-42-guitars-mobile.js
+++ assets/js/visualJS/visuals-42-guitars-mobile.js
@@ -21,11 +21,7 @@ function setup(){
 
 function draw(){
   background(animateColour());
-  var currentXPos = map(rotationZ, 0, 359, 0, windowWidth, true);
-  var currentYPos = map(rotationX, -180, 180, 0, windowHeight, true);
   dragSegment(0, 0, mouseY);
-  console.log("Zrotation, xpos: " +currentXPos);
-  console.log("Xrotation, ypos: " +currentYPos);
   for( var i=0; i<xPositionHistory.length-1; i++) {
     dragSegment(i+1, xPositionHistory[i], yPositionHistory[i]);
   }
